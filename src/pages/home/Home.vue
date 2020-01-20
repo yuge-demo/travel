404e8b76ea3c56c80bd6f690a1fc5c5cca7b9c5b
lianyu <5482897+adees_undefined@user.noreply.gitee.com>
2020-01-20 11:08:08 +0800

    Header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,17 @@
 <template>
-<div>Hello World</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+import HomeHeader from "./components/Header";
 export default {
-    name:'Home'
-}
+  name: "Home",
+  components: {
+    HomeHeader
+  }
+};
 </script>
 
-<style lang="stylus" scoped>
-
-</style>
+<style lang="stylus" scoped></style>
