6c46d41f51b6417b0e46633e67a73d31754a3e7d
lianyu <5482897+adees_undefined@user.noreply.gitee.com>
2020-01-22 15:22:34 +0800

    city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,22 +7,25 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe72e;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe72e;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 <script>
 export default {
   name: "HomeHeader",
-  props:{
-    city:String
+  props: {
+    city: String
   }
 };
 </script>
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl';
+
 .header {
   display: flex;
   line-height: 0.86rem;
@@ -34,16 +37,17 @@ export default {
   width: 0.64rem;
   float: left;
 }
-.back-icon{
-  text-align center
-  font-size 0.4rem
+
+.back-icon {
+  text-align: center;
+  font-size: 0.4rem;
 }
 
 .header-input {
   flex: 1;
   height: 0.64rem;
   line-height: 0.64rem;
-  padding-left 0.2rem
+  padding-left: 0.2rem;
   margin-top: 0.12rem;
   margin-left: 0.2rem;
   background: #ffffff;
@@ -55,9 +59,11 @@ export default {
   width: 1.24rem;
   float: right;
   text-align: center;
+  color: #ffffff;
 }
-.arrow-icon{
-  font-size .4rem
-  margin-left -0.12rem
+
+.arrow-icon {
+  font-size: 0.4rem;
+  margin-left: -0.12rem;
 }
 </style>
