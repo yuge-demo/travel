370e64d4f020dcd42aa9fc7ad797c9a6a502c1c1
lianyu <5482897+adees_undefined@user.noreply.gitee.com>
2020-01-23 16:21:34 +0800

    city-ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,17 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :list="cities" :hot="hotCities"></city-list>
+    <city-alphabet :list="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import CityHeader from "../city/components/Header";
 import CitySearch from "../city/components/Search";
-import CityList   from "../city/components/list";
-import CityAlphabet from "../city/components/Alphabet"
+import CityList from "../city/components/list";
+import CityAlphabet from "../city/components/Alphabet";
 export default {
   name: "City",
   components: {
@@ -19,6 +20,28 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data(){
+      return{
+          cities:{},
+          hotCities:[]
+      }
+  },
+  methods:{
+      getCityInfo(){
+          axios.get('/mock/city.json').then(this.handleGetCityInfoSucc) 
+      },
+      handleGetCityInfoSucc(res){
+          res = res.data
+          if(res.ret && res.data){
+              const data = res.data
+              this.cities = data.cities
+              this.hotCities = data.hotCities
+          }
+      }
+  },
+  mounted() {
+      this.getCityInfo()
   }
 };
 </script>
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,37 +1,35 @@
 <template>
   <ul class="list">
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
-      <li class="item">A</li>
+    <li class="item" v-for="(item , key) of list" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: "CityAlphabet"
+  name: "CityAlphabet",
+  props:{
+      list:Object
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl';
-.list{
-    display flex 
-    flex-direction column
-    justify-content center
-    position absolute
-    right 0
-    top 1.58rem
-    bottom .4rem
-    width .4rem
+
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+  top: 1.58rem;
+  bottom: 0.4rem;
+  width: 0.4rem;
 }
+
 .item {
-    line-height .4rem
-    text-align center
-    color $bgColor
+  line-height: 0.4rem;
+  text-align: center;
+  color: $bgColor;
 }
 </style>
