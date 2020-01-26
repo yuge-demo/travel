dc42914c82e651a91aef9a88cd65c2d92f0a943d
lianyu <5482897+adees_undefined@user.noreply.gitee.com>
2020-01-26 15:49:26 +0800

    detail-header

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>景点详情
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DetailHeader",
+  data() {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    };
+  },
+  methods: {
+    handleScroll() {
+      const top = document.documentElement.scrollTop;
+      if (top > 60) {
+        let opacity = top / 140;
+        opacity = opacity > 1 ? 1 : opacity;
+        this.opacityStyle = {
+          opacity
+        };
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
+      }
+    }
+  },
+  activated() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl';
+
+.header-abs {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.2rem;
+  width: 0.8rem;
+  height: 0.8rem;
+  line-height: 0.8rem;
+  text-align: center;
+  border-radius: 0.4rem;
+  background: rgba(0, 0, 0, 0.8);
+}
+
+.header-abs-back {
+  color: #ffffff;
+  font-size: 0.4rem;
+}
+
+.header-fixed {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 0.86rem;
+  line-height: 0.86rem;
+  text-align: center;
+  color: #ffffff;
+  background: $bgColor;
+  font-size: 0.32rem;
+}
+
+.header-fixed-back {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0.64rem;
+  text-align: center;
+  font-size: 0.4rem;
+  color: #ffffff;
+}
+</style>
